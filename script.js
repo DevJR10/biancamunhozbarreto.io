a2f5c0a1e85ebd4a999e8ca6260c83cc7686eecb
Joao Rodrigo <jralbertini27@gmail.com>
2023-04-09 22:42:18 -0300

    mais alterações

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const fundo1 = document.querySelector('.fundo1')
 const fundo2 = document.querySelector('.fundo2')
 const fundo3= document.querySelector('.fundo3')
+const fundo4= document.querySelector('.fundo4')
 const askBianca= document.querySelector('.askBianca')
 const btn= document.querySelector('.btn')
 const btnYes = document.querySelector('.btnSim')
@@ -55,20 +56,35 @@ const frases3 = [
 
 const imgs = ['imagens/img1.jfif', 'imagens/img13.jfif', 'imagens/img2.jfif', 'imagens/img12.jfif', 'imagens/img6.jfif', 'imagens/img4.jfif', 'imagens/img3.jfif', 'imagens/img7.jfif', 'imagens/img5.jfif', 'imagens/img8.jfif', 'imagens/img10.jfif', 'imagens/img11.jfif', 'imagens/img9.jfif', 'imagens/agua-de-coco.jfif']
 
-const listaFundo3 = [
-   {
-     frase: "E só de lembrar de quando te conheci",
-     imagem: "imagens/img1.jfif"
-   },
-   {
-      frase: "Da nossa primeira conversa",
-      imagem: "imagens/img1.jfif"
-   },
-   {
-      frase: "Do nosso primeiro beijo",
-      imagem: "imagens/img1.jfif"
-   }
- ];
+const imgs2 = [
+'imagens/img1.jfif',
+'imagens/img1.jfif', 
+'imagens/print.jfif', 
+'imagens/img2.jfif', 
+'imagens/pessoalmente.jfif', 
+'imagens/img12.jfif',
+'imagens/primeira-foto.jfif',
+'imagens/primeira-foto.jfif',
+'imagens/ligacao.jfif',
+'imagens/ligacao.jfif',
+'imagens/ligacao.jfif',
+'imagens/ligacao.jfif',
+]
+
+const frasesFundo3 = [
+   'E só de lembrar...',
+   'De quando te conheci...',
+   'De quando te conheci2',
+   'Da nossa primeira conversa pessoalmente',
+   'Da nossa primeira conversa pessoalmente2',
+   'Da nossa primeira foto',
+   'Da nossa primeira foto2',
+   'Da primeira ligação',
+   'Da primeira ligação2',
+   'E só de pensar...',
+   'o quanto ainda vamos viver...',
+   'É de arrepiar.',
+]
 
 
 let i = 0;
@@ -87,12 +103,12 @@ function abrirFundo(){
       }
       p.style.opacity = 1;
       clearInterval()
-   },5500)
+   },2000)
 }
 
 
 let i2 = 0
-let i3 = 0
+
 function abrirFundo2(){
    p2.style.opacity = 0;
    imgsFundo2.style.opacity = 0;
@@ -111,30 +127,88 @@ function abrirFundo2(){
       p2.style.opacity = 1;
       imgsFundo2.style.opacity = 1;
       clearInterval()
+   },2000)
+}
+
+let i3 = 0
+
+function abrirFundo3(){
+   p4.style.opacity = 0;
+   imgsFundo3.style.opacity = 0;
+   fundo3.classList.add('ativo')
+   fundo3.style.opacity = 1;
+   setTimeout((item) => {
+      if(i3 == 0){
+         imgsFundo3.classList.add('absolute')
+      }
+      else if(i3 == 1){
+         imgsFundo3.classList.add('absolute')
+      } 
+      else if(i3 == 2){
+         p4.classList.add('absolute')
+         imgsFundo3.classList.remove('absolute')
+      } 
+      else if(i3 == 3){
+         imgsFundo3.classList.add('absolute')
+         p4.classList.remove('absolute')   
+      }
+      else if(i3 == 4){
+         p4.classList.add('absolute')
+         imgsFundo3.classList.remove('absolute')
+      }
+      else if(i3 == 5){
+         imgsFundo3.classList.add('absolute')
+         p4.classList.remove('absolute')
+      }
+      else if(i3 == 6){
+         p4.classList.add('absolute')
+         imgsFundo3.classList.remove('absolute')
+      }
+      else if(i3 == 7){
+         imgsFundo3.classList.add('absolute')
+         p4.classList.remove('absolute')
+      }
+      else if(i3 == 8){
+         p4.classList.add('absolute')
+         imgsFundo3.classList.remove('absolute')
+      }
+      else if(i3 == 9){
+         imgsFundo3.classList.add('absolute')
+         p4.classList.remove('absolute')
+      }
+      else if(i3 == 10){
+         imgsFundo3.classList.add('absolute')
+         p4.classList.remove('absolute')
+      } 
+      else if(i3 == 11){
+         imgsFundo3.classList.add('absolute')
+         p4.classList.remove('absolute')
+      } else{
+         p4.classList.remove('absolute')
+         p4.textContent = frasesFundo3[i3]
+         imgsFundo3.src = imgs2[i3]
+      }
+      p4.textContent = frasesFundo3[i3]
+      imgsFundo3.src = imgs2[i3]
+      i3++
+      if(i3 > frasesFundo3.length && i3 > imgs2.length){
+         clearTimeout()
+         imgsFundo3.classList.add('remover')
+         abrirFundo4()
+      }
+      p4.style.opacity = 1;
+      imgsFundo3.style.opacity = 1;
+      clearInterval()
    },5500)
 }
 
-let indice = 0
- // Função para exibir a próxima frase e imagem na lista
- function abrirFundo3() {
-   fundo3.classList.add('ativo')
-   // Seleciona o próximo objeto na lista
-   const item = listaDeConteudo[indice];
-   // Exibe a frase correspondente
-   p4.textContent = item.frase;
-   // Define um atraso de 4 segundos antes de exibir a imagem correspondente
-   setTimeout(() => {
-     imgsFundo3.src = item.imagem;
-     // Atualiza o índice para o próximo item na lista
-     indice = (indice + 1) % frases3.length;
-     // Chama a função exibirProximo() novamente após um atraso de 4 segundos
-     setTimeout(abrirFundo3, 4000);
-   }, 5500);
- }
+function abrirFundo4(){
+   fundo4.classList.add('ativo')
+}
 
 btnYes.addEventListener('click', () => {
    abrirFundo()
-   setInterval(abrirFundo, 4500)
+   setInterval(abrirFundo, 1000)
 })
 
 btnNo.addEventListener('click', () => {
@@ -147,11 +221,12 @@ btnNo.addEventListener('click', () => {
 
 btnConfirmar.addEventListener('click', () => {
    abrirFundo2()
-   setInterval(abrirFundo2, 4500)
+   setInterval(abrirFundo2, 1000)
 })
 
 btnConfirmar2.addEventListener('click', () => {
    abrirFundo3();
+   setInterval(abrirFundo3, 4500)
 })
 
 
